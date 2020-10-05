309816a83f3b5188a0a93048d62bd345124cb7d0
peturparkur <61064454+peturparkur@users.noreply.github.com>
2020-10-05 19:42:18 +0100

    Add files via upload

---
+++ Utility/NeuralNetwork/NNTest.cs
@@ -0,0 +1,116 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using System;
+using UnityEngine;
+using PUtil.Plot;
+
+public class NNTest : MonoBehaviour
+{
+    public NeuralNet network;
+    public int[] layerlengths = new int[3];
+    public bool learn = true;
+    public double weightRate = 0.1;
+    public double biasRate = 0.1;
+    //public Vector<double> input;
+    public int epoch = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //creat function arrays
+        int n = layerlengths.Length;
+        Func<double, double>[] activations = new Func<double, double>[n];
+        Func<double, double>[] activationDerivatives = new Func<double, double>[n];
+
+        //input the wanted functions
+        activations[0] = NeuralNetworkUtil.Identity;
+        activationDerivatives[0] = NeuralNetworkUtil.IdentityDerivative;
+        for (int i=1; i<n; i++)
+        {
+            activations[i] = NeuralNetworkUtil.RectifiedLinear;
+            activationDerivatives[i] = NeuralNetworkUtil.RectifiedLinearDerivative;
+        }
+
+        //create the network
+        network = new NeuralNet(layerlengths, activations, activationDerivatives);
+
+        //initialise network with random weights
+        network.Initialise();
+
+        print("the weights are");
+        for (int i = 0; i < network.weights.Length; i++)
+        {
+            print("weight at " + i);
+            print(network.weights[i].ToString());
+        }
+    }
+
+    //we're approximating x^2
+    public double Error(Vector<double> x, Vector<double> y)//x is input, y is output
+    {
+        Vector<double> v = new Vector<double>(x.rows);
+        double sum = 0;
+        v[0] = 0.5 * (x[0] - y[0]) * (x[0] - y[0]);
+        sum += v[0];
+        for(int i=1; i<v.rows; i++)
+        {
+            v[i] = 0.5 * (x[i]*x[i] - y[i]) * (x[i]*x[i] - y[i]);
+            sum += v[i];
+        }
+        return sum;
+
+    }
+
+    //derivative for the x^2 error
+    public Vector<double> ErrorDerivative(Vector<double> x, Vector<double> y)
+    {
+        return y-Vector<double>.EWiseProduct(x,x);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!learn) return;
+
+        //double[] inputs = PlotUtil.Linspace(0, Math.PI * 2.0, 100);
+        double[] inputs = new double[100];
+        for(int i=0; i<100; i++)
+        {
+            if (i % 2 == 0) inputs[i] = 2.0;
+            else inputs[i] = 1.0;
+        }
+
+
+        NeuralNetworkUtil.RandomiseArray(inputs); //randomised the inputs
+        Vector<double>[] inputVector = new Vector<double>[inputs.Length];
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            inputVector[i] = new Vector<double>(1, new double[] { inputs[i] });
+            print("input is  = ");
+            print(inputVector[i].ToString());
+            network.layers[0] = inputVector[i]; //the input vector
+
+            Vector<double> output = network.ForwardEvaluation();
+            print("output is  = ");
+            print(output.ToString());
+            double err = Error(inputVector[i], output);
+            print("the error is = " + err);
+
+            network.Backpropagate(ErrorDerivative, weightRate, biasRate);
+            epoch += 1;
+
+            print("After learning at epoch " + epoch + " the weights are");
+            for (int j = 0; j < network.weights.Length; j++)
+            {
+                print("weight at " + j);
+                print(network.weights[j].ToString());
+            }
+
+            print("________________________________");
+
+
+        }
+    }
+}
---
+++ Utility/NeuralNetwork/NeuralNet.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using PUtil.Operators;
+
+public class NeuralNet
+{
+    public Vector<double>[] layers; //each layer including the input and output vectors
+    public Matrix<double>[] weights; //weights between each vector
+    public Vector<double>[] biases; //biases between each vector
+
+    public Func<Vector<double>, Vector<double>>[] afuncs; //activation functions for each layer - Excluding the input
+    public Func<Vector<double>, Vector<double>>[] dfuncs; //derivative of activation functions - Excluding the input
+
+    public NeuralNet(int[] lengths, Func<double, double>[] _afuncs, Func<double, double>[] _dfuncs)
+    {
+        if (lengths.Length < 2) throw new Exception("At least 2 layers are required for input and output");
+        int n = lengths.Length;
+
+        layers = new Vector<double>[n]; //we want N many layers, this includes in/output
+        weights = new Matrix<double>[n - 1]; //we want N-1 many weights as there's N-1 transitions
+        biases = new Vector<double>[n - 1]; //Again because we have N-1 transitions
+
+        afuncs = new Func<Vector<double>, Vector<double>>[n]; //because we require activation function for input - might be changed
+        dfuncs = new Func<Vector<double>, Vector<double>>[n]; //same as above
+
+        for(int i=0; i<n; i++)
+        {
+            layers[i] = new Vector<double>(lengths[i]);
+            afuncs[i] = NeuralNetworkUtil.CreateVectorFunction(_afuncs[i]); //this just translates the double function to do this activation for each of the elements of the vector
+            dfuncs[i] = NeuralNetworkUtil.CreateVectorFunction(_dfuncs[i]); //the indexies as follows W(i) * A(i) + B(i) = V(i+1) => F[i+1](V(i+1)) = A(i+1)
+
+            if (i < 1) continue;
+
+            weights[i-1] = new Matrix<double>(lengths[i], lengths[i-1]); //compatible to multiple as W(i) * A(i) + B(i) = V(i+1), where W is weights, V are the un-activated layer, B is bias, A is activated layer
+            biases[i-1] = new Vector<double>(lengths[i]); //same as above
+        }
+    }
+
+    public void Initialise(int seed = 0)
+    {
+        Random rand;
+        if (seed == 0) rand = new Random();
+        else rand = new Random(seed);
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].rows; j++)
+            {
+                for (int k = 0; k < weights[i].columns; k++)
+                {
+                    weights[i][j, k] = rand.NextDouble() / 10.0;
+                }
+            }
+        }
+    }
+
+    public Vector<double> EvaluateLayer(int i)
+    {
+        //W(i) * A(i) + B(i) = V(i+1) => A(i+1) = F[i](V(i+1))
+        //where W is weights, V are the un-activated layer, B is bias, A is activated layer
+        return weights[i] * afuncs[i](layers[i]) + biases[i]; //this was written in the initialise bit
+    }
+
+    public Vector<double> ForwardEvaluation() //here we assumed that layers[0] is already is set
+    {
+        int n = layers.Length-1;
+        for (int i=0; i<n; i++)
+        {
+            if(i == 0) { layers[i + 1] = weights[i] * afuncs[i](layers[i]) + biases[i]; continue; } //because for now we assumed that the first layer "the input", does require activation
+
+            layers[i + 1] = EvaluateLayer(i); //just the usual forward evaluation, but we store in layers the un-activated vectors
+        }
+        return afuncs[n](layers[n]); //return the activated output layer
+    }
+
+    public Vector<double> SigmaEnd(Func<Vector<double>, Vector<double>, Vector<double>> dCost) //DCost is the derivative of the cost function
+    {
+        //since we assumed that the input/first layer is already activated
+        //Cost(input, output) = Error
+        int n = layers.Length - 1;
+        Vector<double> costs = dCost(afuncs[0](layers[0]), afuncs[n](layers[n])); //this is the partial derivatives of the cost function with respect to the outputs
+        return new Vector<double>(Matrix<double>.EWiseProduct(costs, dfuncs[n](layers[n]))); //because the last layers is the non-activated output function
+    }
+
+    public Vector<double> SigmaInner(int l, Vector<double> sigma) //l is the layer
+    {
+        if (l >= layers.Length - 1) throw new Exception("this is the last layer, can't apply method");
+        return new Vector<double>(Matrix<double>.EWiseProduct(Matrix<double>.Transpose(weights[l+1]) * sigma, dfuncs[l+1](layers[l+1])));
+    }
+
+    public void UpdateWeightBiases(Vector<double>[] sigmas, double weightRate, double biasRate)
+    {
+        int n = weights.Length;
+        for(int i=0; i<n; i++)
+        {
+            biases[i] -= sigmas[i] * biasRate;
+            weights[i] -= (sigmas[i] * Matrix<double>.Transpose(afuncs[i](layers[i]))) * weightRate;
+        }
+    }
+
+    public void Backpropagate(Func<Vector<double>, Vector<double>, Vector<double>> dCost, double weightRate, double biasRate) //we need the cost function to backpropagate with respect to
+    {
+        ForwardEvaluation();
+        int n = weights.Length;
+        Vector<double>[] sigmas = new Vector<double>[n];
+        sigmas[n - 1] = SigmaEnd(dCost);
+        for(int i=n-2; i>=0; i--) //we go backwards
+        {
+            sigmas[i] = SigmaInner(i, sigmas[i + 1]);
+        }
+        UpdateWeightBiases(sigmas, weightRate, biasRate); //finished backpropagation
+    }
+
+}
