af99cc4a6d036f6169cdaece1016f96e09a48e57
peturparkur <61064454+peturparkur@users.noreply.github.com>
2020-10-05 19:43:10 +0100

    Add files via upload

---
+++ FluidSim/FluidRun.cs
@@ -0,0 +1,156 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FluidRun : MonoBehaviour
+{
+    FluidSimFlat fluid;
+    Matrix<float> matrix;
+    public float c = 1;
+    public float damp = 1f;
+    public float dx = 0.2f;
+    public float dy = 0.2f;
+    public float dt = 0.01f;
+
+    public int sizeX = 5;
+    public int sizeY = 5;
+
+    //public UnityEngine.Object prefab; //the thing that we will visualise stuff with
+    //Transform[] vis;
+    //public Mesh mesh;
+    public float visScale = 5f;
+    public float heightScale = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        matrix = new Matrix<float>(sizeX, sizeY);
+        matrix[(int)Math.Ceiling((double)(sizeX/2)), (int)Math.Ceiling((double)(sizeX / 2))] = 5;
+        fluid = new FluidSimFlat(matrix, matrix, dx,dy);
+        //Debug.Log(matrix.ToString());
+
+        //visualise
+        /*
+        vis = new Transform[sizeX * sizeY];
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                int t = i * sizeY + j;
+                GameObject go = (GameObject)Instantiate(prefab, new Vector3(), Quaternion.identity, transform);
+                vis[t] = go.transform;
+                vis[t].position = new Vector3(i * dx * visScale, matrix[i, j], j * dy * visScale);
+            }
+        }*/
+        Mesh mesh = new Mesh();
+        Vector3[] verts = new Vector3[sizeX * sizeY];
+        int[] tris = new int[(sizeX - 1) * (sizeY - 1) * 2 * 3]; //amount of tris
+        //Debug.Log("Max Tris = " + tris.Length/3);
+        int n = 0;
+        for (int i=0; i<sizeX; i++)
+        {
+            for(int j=0; j<sizeY; j++)
+            {
+                int t = i * sizeY + j;
+                verts[t] = new Vector3(i * dx*visScale, matrix[i, j]*heightScale, j * dy*visScale);
+                //Debug.Log("T = " + t);
+                //Debug.Log("N = " + n);
+
+                if (i < sizeX-1)
+                {
+                    if(j<sizeY-1)
+                    {
+
+                        //this was created anticlockwise
+                        /*
+                        Debug.Log("tris numb = " + (n * 2));
+                        tris[n*2 * 3] = t;
+                        tris[n*2 * 3 + 1] = t + (1 * sizeY);
+                        tris[n*2 * 3 + 2] = t + (1 * sizeY) + 1;
+                        //Debug.Log("v1 = " + t);
+                        //Debug.Log("v2 = " + (t + (i * sizeY)));
+                        //Debug.Log("v3 = " + (t + (i * sizeY) + j));
+
+                        tris[(n*2 + 1) * 3] = t;
+                        tris[(n*2 + 1) * 3 + 1] = t + (1 * sizeY) + 1;
+                        tris[(n*2 + 1) * 3 + 2] = t + 1;
+                        //Debug.Log("u1 = " + t);
+                        //Debug.Log("u2 = " + (t + (i * sizeY) + j));
+                        //Debug.Log("u3 = " + (t + j));*/
+
+                        tris[n * 2 * 3] = t;
+                        tris[n * 2 * 3 + 1] = t + (1 * sizeY) + 1;
+                        tris[n * 2 * 3 + 2] = t + (1 * sizeY);
+
+                        tris[(n * 2 + 1) * 3] = t;
+                        tris[(n * 2 + 1) * 3 + 1] = t + 1;
+                        tris[(n * 2 + 1) * 3 + 2] = t + (1 * sizeY) + 1;
+
+                        n++;
+                        //Debug.Log("tris created = " + (n * 2));
+                    }
+                }
+            }
+        }
+
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        MeshRenderer r = this.gameObject.AddComponent<MeshRenderer>();
+        r.sharedMaterial = new Material(Shader.Find("Standard"));
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = mesh;
+
+
+        
+        //fluid.Initialise(values, values, c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fluid.Step(dt, c,damp);
+        matrix = fluid.val;
+        for(int i=(int)(sizeX*0.75); i<sizeX; i++)
+        {
+            for(int j=40; j<80; j++)
+            {
+                fluid.val[i, j] = 0f;
+                //matrix[i, j] = 2f;               
+            }
+            for(int j=120; j<160; j++)
+            {
+                fluid.val[i, j] = 0f;
+                //matrix[i, j] = 2f;               
+            }
+        }
+        //Debug.Log(matrix.ToString());
+
+        /*for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                int t = i * sizeY + j;
+                //GameObject go = (GameObject)Instantiate(prefab, new Vector3(), Quaternion.identity, transform);
+                //vis[t] = go.transform;
+                vis[t].position = new Vector3(i * dx * visScale, matrix[i, j], j * dy * visScale);
+            }
+        }*/
+        MeshFilter filter = gameObject.GetComponent<MeshFilter>();
+        Mesh mesh = filter.mesh;
+        Vector3[] verts = mesh.vertices;
+
+        for (int i=0; i<sizeX; i++)
+        {
+            for(int j=0; j<sizeY; j++)
+            {
+                //Debug.DrawRay(new Vector3(i * dx * visScale, 0f, j * dy * visScale), Vector3.up * (matrix[i,j] + 1f), Color.blue);
+                verts[i*sizeY+j] = new Vector3(i * dx * visScale, matrix[i, j] * heightScale, j * dy * visScale);
+            }
+        }
+        mesh.vertices = verts;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ FluidSim/FluidSimFlat.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+public class FluidSimFlat
+{
+    public Matrix<float> val; //the matrix represents the flat 2D surface and each value is a height/density value associated to each position at T
+    Matrix<float> prevVal; //values at T-1
+    Matrix<float> pastVal; //values at T-2
+    public float dx; //the defined difference of each index of vals
+    public float dy;
+    //public float dt; //the defined time difference between val and prevVal
+    //public float c;
+
+    //wave equation is defined as ddot(u) = c^2 * delta^2(u); where delta is the Del or Delta operator (gradient)
+
+    public FluidSimFlat(Matrix<float> _val, Matrix<float> _prevVal, float _dx, float _dy)
+    {
+        val = new Matrix<float>(_val);
+        prevVal = new Matrix<float>(_val);
+        //c = _c;
+        dx = _dx;
+        dy = _dy;
+    }
+
+    public FluidSimFlat(int sizeX, int sizeY, float _dx, float _dy)
+    {
+        val = new Matrix<float>(sizeX, sizeY);
+        prevVal = new Matrix<float>(sizeX, sizeY);
+        pastVal = new Matrix<float>(sizeX, sizeY);
+        //c = _c;
+        dx = _dx;
+        dy = _dy;
+    }
+
+    /*public void Initialise(int sizeX, int sizeY, float _c)
+    {
+        val = new Matrix<float>(sizeX, sizeY);
+        prevVal = new Matrix<float>(sizeX, sizeY);
+        pastVal = new Matrix<float>(sizeX, sizeY);
+        c = _c;
+    }*/
+
+    /*public void Initialise(Matrix<float> _val, Matrix<float> _prevVal, float _c)
+    {
+        val = new Matrix<float>(_val);
+        prevVal = new Matrix<float>(_val);
+        c = _c;
+    }*/
+
+    public float Accelaration(int x, int y, float c, float dt, float d) //this calculates ddot(u), the accelaration of u
+    {
+        //X,Y represent the row and column position respectively
+        //if val.rows = n then we only allow x to be n-2 instead of the n-1 which would be the last value
+        int n = val.rows;
+        int m = val.columns;
+
+        if (y >= m - 1 || y <= 0) throw new Exception("Y is on/outside the boundary");
+        if (x >= n - 1 || x <= 0) throw new Exception("X is on/outside the boundary");
+
+        //since we work in 2D delta^2 ==> d(u)/dx^2 + d(u)/dy^2
+
+        float nx = val[x + 1, y] - 2f * val[x, y] + val[x - 1, y]; //x derivative
+        float ny = val[x, y + 1] - 2f * val[x, y] + val[x, y - 1]; //y derivative
+
+        float dr2 = dx * dx + dy * dy;
+        float dr = (float)Math.Sqrt(dr2);
+        float fx = (val[x - 1, y - 1] - 2f * val[x, y] + val[x + 1, y + 1]);
+        float fy = (val[x + 1, y - 1] - 2f * val[x, y] + val[x - 1, y + 1]);
+        //float mult = (dx * dx) / dr2;
+
+        float average = (val[x + 1, y] + val[x - 1, y])/dx + (val[x, y + 1] + val[x, y - 1])/dy + (val[x+1,y+1] + val[x+1,y-1] + val[x-1,y+1] + val[x-1,y-1])/dr;
+        average = average / 8f;
+
+        return c * c * (nx/(dx*dx) + ny/(dy*dy) + (fx+fy)/dr2) - d*(val[x,y] - prevVal[x,y])/dt; //this is c^2 * delta^2(u);
+    }
+
+    public Matrix<float> AccelerationMatrix(float c, float dt, float d)
+    {
+        Matrix<float> acc = new Matrix<float>(val.rows, val.columns);
+        for(int i=1; i<val.rows-1; i++)
+        {
+            for(int j=1; j<val.columns-1; j++)
+            {
+                acc[i, j] = Accelaration(i, j, c,dt,d);
+            }
+        }
+        return acc;
+    }
+
+    public void Step(float dt, float c, float d)
+    {
+        Matrix<float> acc = AccelerationMatrix(c,dt,d);
+        Matrix<float> temp = new Matrix<float>(val);
+        for(int i=1; i<val.rows-1; i++)
+        {
+            for(int j=1; j<val.columns-1; j++)
+            {
+                val[i, j] = 2f * val[i, j] - prevVal[i, j] + c * c * acc[i, j] * dt * dt; //verlet method
+            }
+        }
+        prevVal = temp; //to assign the pervious values
+    }
+
+}
